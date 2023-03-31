beb196c2281087f7e4c7e8c2affb86c1237ce2b4
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-03-31 18:48:52 +0200

    :building_construction: Create Home component

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+// Import css module
+import styles from './home.module.css'
+
+
+function Home() {
+
+    return (
+        <>
+            <main>Home</main>
+        </>
+    )
+
+}
+
+export default Home
