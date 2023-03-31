607e21401311603559bfe5f3b66a8b792b2cd5a8
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-03-31 18:49:41 +0200

    :building_construction: Create Signin component

---
+++ src/pages/Signin/index.jsx
@@ -0,0 +1,15 @@
+// Import css module
+import styles from './signin.module.css'
+
+
+function Signin() {
+
+    return (
+        <>
+            <main>Signin</main>
+        </>
+    )
+
+}
+
+export default Signin
