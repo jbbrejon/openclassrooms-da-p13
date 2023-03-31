47a44299926b3fd9c1a43a7ed240bd4b8f149fa2
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-03-31 18:48:32 +0200

    :building_construction: Create Error component

---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,15 @@
+// Import css module
+import styles from './error.module.css'
+
+
+function Error() {
+
+    return (
+        <>
+            <main>Error</main>
+        </>
+    )
+
+}
+
+export default Error
