4b650771f93d8d02e532baba9ce8745293084ffe
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-03-31 18:49:20 +0200

    :building_construction: Create Profile component

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,15 @@
+// Import css module
+import styles from './profile.module.css'
+
+
+function Profile() {
+
+    return (
+        <>
+            <main>Profile</main>
+        </>
+    )
+
+}
+
+export default Profile
