75e7bb7b68f229d9adcea8ff285cd0b4cf0711bc
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-03-31 18:47:43 +0200

    :building_construction: Create Nav component

---
+++ src/layouts/Nav/index.jsx
@@ -0,0 +1,18 @@
+// Import site logo
+import logo from '../../assets/argentBankLogo.png'
+
+// Import css module
+import styles from './nav.module.css'
+
+
+function Nav() {
+
+    return (
+        <>
+            <nav>Nav</nav>
+        </>
+    )
+
+}
+
+export default Nav
