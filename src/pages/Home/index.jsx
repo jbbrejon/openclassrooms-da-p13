1e1d1d0a8ed62d1707114ecfbc0a5eec18e3a2ad
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-04-01 09:12:24 +0200

    :sparkles: Update Home component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,7 @@
+// Import components
+import Hero from '../../components/Hero'
+import Features from '../../components/Features'
+
 // Import css module
 import styles from './home.module.css'
 
@@ -6,7 +10,10 @@ function Home() {
 
     return (
         <>
-            <main>Home</main>
+            <main>
+                <Hero></Hero>
+                <Features></Features>
+            </main>
         </>
     )
 
