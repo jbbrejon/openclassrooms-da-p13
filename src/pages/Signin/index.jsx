86c73317a3060363b6b1329d4400622296dda705
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-04-01 09:12:54 +0200

    :sparkles: Update Signin component

--- src/pages/Signin/index.jsx
+++ src/pages/Signin/index.jsx
@@ -1,12 +1,55 @@
+// Import modules
+import { useState } from 'react';
+
 // Import css module
 import styles from './signin.module.css'
 
 
 function Signin() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(` Email: ${email}, Password: ${password}`)
+    }
 
     return (
         <>
-            <main>Signin</main>
+            <main className={styles["bg-dark"]}>
+                <section className={styles["sign-in-content"]}>
+                    <i className={`${styles["sign-in-icon"]} fa fa-user-circle`}></i>
+                    <h1>Sign In</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className={styles["input-wrapper"]} >
+                            <label htmlFor="username">Username</label>
+                            <input
+                                type="text"
+                                id="username"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)} />
+
+                        </div>
+                        <div className={styles["input-wrapper"]}>
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <div className={styles["input-remember"]}>
+                            <input type="checkbox" id="remember-me" />
+                            <label htmlFor="remember-me">Remember me</label>
+                        </div>
+
+                        <button type="submit" className={styles["sign-in-button"]}>Sign In</button>
+
+                    </form>
+                </section>
+            </main>
         </>
     )
 
