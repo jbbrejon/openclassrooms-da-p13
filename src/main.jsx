12beecf8e06d0e637865eb8602e488786041d502
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-04-01 17:43:17 +0200

    :wrench: Add Redux store provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 
 // Import router
 import Router from '../src/utils/router'
+
+// Import Redux store
+import store from './utils/store'
 
 // Import global css
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={Router} />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={Router} />
+    </React.StrictMode>
+  </Provider>,
 )
