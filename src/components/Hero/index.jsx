4bb4be999190920461ee39be09fb3b8de585a9a7
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-04-01 09:10:09 +0200

    :sparkles: Create Hero component

---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,21 @@
+import styles from './hero.module.css'
+
+function Hero() {
+    return (
+        <>
+            <main>
+                <div className={styles.hero}>
+                    <section className={styles["hero-content"]}>
+                        <h2 className="sr-only">Promoted Content</h2>
+                        <p className={styles.subtitle}>No fees.</p>
+                        <p className={styles.subtitle}>No minimum deposit.</p>
+                        <p className={styles.subtitle}>High interest rates.</p>
+                        <p className={styles.test}>Open a savings account with Argent Bank today!</p>
+                    </section>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Hero
