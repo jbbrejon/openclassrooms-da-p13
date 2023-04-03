7fa9357a7ab31f85efa0520034e8209d6790162e
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-04-03 10:49:10 +0200

    :sparkles: Display user name from profile state

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,12 +1,23 @@
+// Import modules
+import { useSelector } from 'react-redux';
+
+// Import Redux selectors
+import { selectProfile } from '../../utils/selectors'
+
 // Import css module
 import styles from './profile.module.css'
 
 
 function Profile() {
-
+    const profile = useSelector(selectProfile);
     return (
         <>
-            <main>Profile</main>
+            <main className={styles["bg-dark"]}>
+                <div className={styles.header} >
+                    <h1>Welcome back<br />{profile.firstName} {profile.lastName} !</h1>
+                </div>
+
+            </main >
         </>
     )
 
