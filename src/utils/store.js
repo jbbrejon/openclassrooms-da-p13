f83abddced5a599de7c2da9ba3abc39962d0f967
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-04-03 10:47:22 +0200

    :sparkles: add profile reducer

--- src/utils/store.js
+++ src/utils/store.js
@@ -3,10 +3,12 @@ import { configureStore } from '@reduxjs/toolkit'
 
 // Import Redux slices
 import authReducer from '../features/auth/authSlice'
+import profileReducer from '../features/profile/profileSlice'
 
 
 export default configureStore({
     reducer: {
         auth: authReducer,
+        profile: profileReducer,
     },
 })
