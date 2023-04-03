b54443483787457f49a426b55ad115715929cccb
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-04-03 11:46:28 +0200

    :file_cabinet: Create account data (mock)

---
+++ src/data/accountMock.js
@@ -0,0 +1,23 @@
+// Mock
+const accountMock = [
+    {
+        id: 0,
+        title: "Argent Bank Checking (x8349)",
+        amount: "$2,082.79",
+        description: "Available Balance"
+    },
+    {
+        id: 1,
+        title: "Argent Bank Savings (x6712)",
+        amount: "$10,928.42",
+        description: "Available Balance"
+    },
+    {
+        id: 2,
+        title: "Argent Bank Credit Card (x8349)",
+        amount: "$184.30",
+        description: "Current Balance"
+    }
+]
+
+export default accountMock
