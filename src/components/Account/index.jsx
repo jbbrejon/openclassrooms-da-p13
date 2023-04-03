a168154a7a03a72a276dd77f042caa659c657693
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-04-03 11:47:02 +0200

    :sparkles: Create Account component

---
+++ src/components/Account/index.jsx
@@ -0,0 +1,20 @@
+import styles from './account.module.css'
+
+function Account({ title, amount, description }) {
+    return (
+        <>
+            <section className={styles["account"]} >
+                <div className={styles["account-content-wrapper"]} >
+                    <h3 className={styles["account-title"]}>{title}</h3>
+                    <p className={styles["account-amount"]} >{amount}</p>
+                    <p className={styles["account-amount-description"]} >{description}</p>
+                </div>
+                <div className={styles["account-content-wrapper cta"]}>
+                    <button className={styles["transaction-button"]}>View transactions</button>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Account
