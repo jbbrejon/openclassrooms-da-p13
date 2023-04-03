514871f394e346bbed138f3d1fd95944ffbcfb2c
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-04-03 10:46:52 +0200

    :sparkles: create profile reducer

---
+++ src/features/profile/profileSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    firstName: null,
+    lastName: null,
+}
+
+export const profileSlice = createSlice({
+    name: 'profile',
+    initialState,
+    reducers: {
+        update: (draft, action) => {
+            const firstName = action.payload.firstName;
+            draft.firstName = firstName;
+            const lastName = action.payload.lastName;
+            draft.lastName = lastName;
+            return
+        },
+        reset: () => {
+            return initialState
+        },
+
+    },
+})
+
+export const { update, reset } = profileSlice.actions
+
+export default profileSlice.reducer
