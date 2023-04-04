c090997b0dedae05ec31b4cbaa9817bdb7e25e6d
Jean-Baptiste BREJON <jbbrejon+gthb@outlook.fr>
2023-04-04 13:32:32 +0200

    :wrench: Improve error handling

--- src/utils/apiLogin.js
+++ src/utils/apiLogin.js
@@ -3,12 +3,14 @@ import axios from "axios";
 const baseUrl = "http://localhost:3001/api/v1/user/"
 
 export default async function apiLogin(email, password) {
-    let url = `${baseUrl}login`
+    let url = `${baseUrl}login`;
+    let response;
+
     try {
-        const response = await axios.post(url, { email: email, password: password });
+        response = await axios.post(url, { email: email, password: password });
         return response
-    } catch (error) {
-        console.error(error);
-        return error
+    } catch (e) {
+        console.error(e);
+        return e.response;
     }
 }
